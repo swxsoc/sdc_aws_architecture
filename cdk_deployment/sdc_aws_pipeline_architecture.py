4d3cb00cfe5ed18d25b1e5a708b2ea8dbd032f5a
Damian Barrous Dume <damianbarrous@gmail.com>
2022-08-03 17:50:20 +0100

    Add Public and Private ECR Repos to CDK (#3)

--- cdk_deployment/sdc_aws_pipeline_architecture.py
+++ cdk_deployment/sdc_aws_pipeline_architecture.py
@@ -1,7 +1,9 @@
-from aws_cdk import Stack, aws_s3
+from aws_cdk import Stack, aws_s3, aws_ecr, Tags
 from constructs import Construct
 from . import vars
 import logging
+import os
+from datetime import datetime
 
 
 class SDCAWSPipelineArchitectureStack(Stack):
@@ -9,10 +11,65 @@ class SDCAWSPipelineArchitectureStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # Iterate through the S3 Buckets List and Create the Buckets
-        for bucket in vars.BUCKETS:
+        for bucket in vars.BUCKET_LIST:
 
+            # Initiate Bucket
             s3_bucket = aws_s3.Bucket(
                 self, f"aws_sdc_{bucket}_bucket", bucket_name=bucket
             )
 
+            # Apply Tags
+            self._apply_standard_tags(s3_bucket)
+
+            # Log Result
             logging.info(f"Created the {s3_bucket} S3 Bucket")
+
+        # Iterate through the Private ECR Repos and initiate
+        for ecr_repo in vars.ECR_PRIVATE_REPO_LIST:
+
+            # Initiate Repo
+            private_ecr_repo = aws_ecr.Repository(
+                self, f"aws_sdc_{ecr_repo}_private_repo", repository_name=ecr_repo
+            )
+
+            # Apply Tags
+            self._apply_standard_tags(private_ecr_repo)
+
+            # Log Result
+            logging.info(f"Created the {private_ecr_repo} Private ECR Repo")
+
+        # Iterate through the Public ECR Repos and initiate
+        for ecr_repo in vars.ECR_PUBLIC_REPO_LIST:
+
+            # Initiate Repo
+            public_ecr_repo = aws_ecr.CfnPublicRepository(
+                self, f"aws_sdc_{ecr_repo}_public_repo", repository_name=ecr_repo
+            )
+
+            # Apply Tags
+            Tags.of(public_ecr_repo).add(
+                "Purpose", "SWSOC Pipeline", apply_to_launched_instances=True
+            )
+
+            # Log Result
+            logging.info(f"Created the {public_ecr_repo} Public ECR Repo")
+
+    def _apply_standard_tags(self, construct):
+        """
+        This function applies the default tags to the different resources created
+        """
+
+        # Standard Purpose Tag
+        Tags.of(construct).add(
+            "Purpose", "SWSOC Pipeline", apply_to_launched_instances=True
+        )
+
+        # Standard Last Modified Tag
+        Tags.of(construct).add("Last Modified", str(datetime.today()))
+
+        # Standard Environment Tag
+        Tags.of(construct).add("Environment", "Production")
+
+        # Git Version Tag If It Exists
+        if os.getenv("GIT_TAG"):
+            Tags.of(construct).add("Version", os.getenv("GIT_TAG"))
--- cdk_deployment/vars.py
+++ cdk_deployment/vars.py
@@ -8,12 +8,24 @@ SPANI_BUCKET_NAME = "hermes-spani"
 NEMISIS_BUCKET_NAME = "hermes-nemisis"
 EEA_BUCKET_NAME = "hermes-eea"
 MERIT_BUCKET_NAME = "hermes-merit"
+SORTING_LAMBDA_BUCKET_NAME = "swsoc-sorting-lambda"
 
 # List of Buckets to be Created
-BUCKETS = [
+BUCKET_LIST = [
     INCOMING_BUCKET_NAME,
     SPANI_BUCKET_NAME,
     NEMISIS_BUCKET_NAME,
     EEA_BUCKET_NAME,
     MERIT_BUCKET_NAME,
+    SORTING_LAMBDA_BUCKET_NAME,
 ]
+
+# ECR Repository Names
+PROCESSING_LAMBDA_ECR_NAME = "sdc_aws_processing_lambda"
+SWSOC_DOCKER_BASE_ECR_NAME = "swsoc-docker-lambda-base"
+
+# Public ECR Repo List (This is deployed to all regions)
+ECR_PUBLIC_REPO_LIST = [SWSOC_DOCKER_BASE_ECR_NAME]
+
+# Private ECR Repo List (This is only deployed to selected region)
+ECR_PRIVATE_REPO_LIST = [PROCESSING_LAMBDA_ECR_NAME]
