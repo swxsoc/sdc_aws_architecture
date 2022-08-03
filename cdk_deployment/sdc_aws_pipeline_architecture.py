98ae804c5ac5d39320df95f7d9c88a21dffd869f
Damian Barrous Dume <damianbarrous@gmail.com>
2022-08-03 15:58:45 +0100

    Add S3 Buckets to the Pipeline Stack (#2)

--- cdk_deployment/sdc_aws_pipeline_architecture.py
+++ cdk_deployment/sdc_aws_pipeline_architecture.py
@@ -1,7 +1,18 @@
-from aws_cdk import Stack
+from aws_cdk import Stack, aws_s3
 from constructs import Construct
+from . import vars
+import logging
 
 
 class SDCAWSPipelineArchitectureStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # Iterate through the S3 Buckets List and Create the Buckets
+        for bucket in vars.BUCKETS:
+
+            s3_bucket = aws_s3.Bucket(
+                self, f"aws_sdc_{bucket}_bucket", bucket_name=bucket
+            )
+
+            logging.info(f"Created the {s3_bucket} S3 Bucket")
---
+++ cdk_deployment/vars.py
@@ -0,0 +1,19 @@
+"""
+This contains variables used throughout the SDC Pipeline
+"""
+
+# S3 Bucket Names
+INCOMING_BUCKET_NAME = "swsoc-incoming"
+SPANI_BUCKET_NAME = "hermes-spani"
+NEMISIS_BUCKET_NAME = "hermes-nemisis"
+EEA_BUCKET_NAME = "hermes-eea"
+MERIT_BUCKET_NAME = "hermes-merit"
+
+# List of Buckets to be Created
+BUCKETS = [
+    INCOMING_BUCKET_NAME,
+    SPANI_BUCKET_NAME,
+    NEMISIS_BUCKET_NAME,
+    EEA_BUCKET_NAME,
+    MERIT_BUCKET_NAME,
+]
