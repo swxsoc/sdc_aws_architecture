1d0a808408f6a16ff37b58b7a6f9a003e314f371
Damian Barrous-Dume <damianbarrous@gmail.com>
2022-08-22 13:44:18 +0100

    Update processing function architecture

--- cdk_deployment/sdc_aws_processing_lambda.py
+++ cdk_deployment/sdc_aws_processing_lambda.py
@@ -1,5 +1,15 @@
 import os
-from aws_cdk import Stack, aws_lambda, aws_ecr, Duration
+from datetime import datetime
+from sys import prefix
+from aws_cdk import (
+    Stack,
+    aws_lambda,
+    aws_ecr,
+    Duration,
+    aws_s3,
+    aws_s3_notifications,
+    Tags,
+)
 from constructs import Construct
 import logging
 from . import vars
@@ -35,4 +45,49 @@ class SDCAWSProcessingLambdaStack(Stack):
         # Grant Access to Repo
         ecr_repository.grant_pull_push(sdc_aws_processing_function)
 
+        # Apply Standard Tags to CW Event
+        self._apply_standard_tags(sdc_aws_processing_function)
+
+        # Grant Access to Buckets
+        for bucket in vars.BUCKET_LIST:
+            # Get the incoming bucket from S3
+            lambda_bucket = aws_s3.Bucket.from_bucket_name(
+                self, f"aws_sdc_{bucket}", bucket
+            )
+            lambda_bucket.grant_read_write(sdc_aws_processing_function)
+
+            # Add Trigger to the Bucket to call Lambda
+            lambda_bucket.add_event_notification(
+                aws_s3.EventType.OBJECT_CREATED,
+                aws_s3_notifications.LambdaDestination(sdc_aws_processing_function),
+                prefix="unprocessed/",
+            )
+
         logging.info("Function created successfully: %s", sdc_aws_processing_function)
+
+    def _apply_standard_tags(self, construct):
+        """
+        This function applies the default tags to the different resources created
+        """
+
+        # Standard Purpose Tag
+        Tags.of(construct).add(
+            "Purpose", "SWSOC Pipeline", apply_to_launched_instances=True
+        )
+
+        # Standard Last Modified Tag
+        Tags.of(construct).add("Last Modified", str(datetime.today()))
+
+        # Environment Name
+        environment_name = (
+            "Production"
+            if os.getenv("CDK_ENVIRONMENT") == "PRODUCTION"
+            else "Development"
+        )
+
+        # Standard Environment Tag
+        Tags.of(construct).add("Environment", environment_name)
+
+        # Git Version Tag If It Exists
+        if os.getenv("GIT_TAG"):
+            Tags.of(construct).add("Version", os.getenv("GIT_TAG"))
